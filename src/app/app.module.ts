9659ae16f73155546eb898fb1c17c242511b6301
Dennis-Schneider <107464538+Dennis-Schneider@users.noreply.github.com>
2023-05-21 22:05:42 +0200

    menu p1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { StartSectionComponent } from './start-section/start-section.component';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    StartSectionComponent
+    StartSectionComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
