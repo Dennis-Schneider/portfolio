01053c21d5e303f22b42ebe66bd1cfd677f80592
Dennis-Schneider <107464538+Dennis-Schneider@users.noreply.github.com>
2023-05-22 21:54:38 +0200

    menu final?

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -7,8 +7,7 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
   menuVisible = false;
-  toggleActiveState() {
+  toggleMenu() {
     this.menuVisible = !this.menuVisible;
-    console.log(this.menuVisible);
   }
 }
