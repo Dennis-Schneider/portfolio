4744011755d99d65e1944becc0f0ef70c327746b
dennis-schneider <dennis@its-visible.de>
2023-08-15 12:12:49 +0200

    imprint fixes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LegalNoticeComponent } from './legal-notice/legal-notice.component';
+import { StartSectionComponent } from './start-section/start-section.component';
 
 const routes: Routes = [
+  { path: '', component: StartSectionComponent },
   { path: 'legal-notice', component: LegalNoticeComponent },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes, { scrollPositionRestoration: 'enabled' }),
+  ],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
